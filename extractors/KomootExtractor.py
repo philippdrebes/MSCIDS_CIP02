d98be78114250fb8be99428775c91dbf029fbe2a
Philipp Drebes <philipp.drebes@gmail.com>
2023-04-15 14:31:54 +0200

    removed Extractor Base class

--- extractors/KomootExtractor.py
+++ extractors/KomootExtractor.py
@@ -1,10 +1,9 @@
 from selenium.webdriver.common.by import By
 
-from extractors.BaseExtractor import BaseExtractor
 import extractors.Selenium
 
 
-class KomootExtractor(BaseExtractor):
+class KomootExtractor:
     base_url = 'https://www.komoot.com/discover/hiking-trails'
 
     page_objects = {
@@ -12,11 +11,9 @@ class KomootExtractor(BaseExtractor):
             'filter_btn': '//*[@id="pageMountNode"]/div/div[3]/div[2]/div/div/main/section[1]/div[2]/div/div[1]/div/div/button[1]',
             'filter_country_ch_lbl': '//*[@id="pageMountNode"]/div/div[3]/div[2]/div/div/main/section[1]/div[2]/div/div[2]/div/div[2]/div/div[13]/label',
             'next_page_btn': '//*[@id="pageMountNode"]/div/div[3]/div[2]/div/div/main/section[1]/div[2]/div/div[3]/div[3]/button',
-            # '': '',
         },
         'region': {
             'tour_title_attr': 'data-test-id="tour_title"',
-            # '':'',
         },
         'tour': {
             'title_lbl': '//*[@id="title"]/div[2]/h1',
@@ -26,16 +23,13 @@ class KomootExtractor(BaseExtractor):
             'speed_lbl': '//*[@id="pageMountNode"]/div/div[3]/div[2]/div/div/div/div/div[1]/div/div/div/div[2]/div/div[2]/div/div[4]/div/div/div[2]/div/div/span',
             'elevation_up_lbl': '//*[@id="pageMountNode"]/div/div[3]/div[2]/div/div/div/div/div[1]/div/div/div/div[2]/div/div[2]/div/div[5]/div/div/div[2]/span',
             'elevation_down_lbl': '//*[@id="pageMountNode"]/div/div[3]/div[2]/div/div/div/div/div[1]/div/div/div/div[2]/div/div[2]/div/div[6]/div/div/div[2]/span',
-            # '':'',
         }
     }
 
     def __init__(self):
-        super().__init__("Komoot")
+        print('Starting Komoot extraction')
 
     def extract(self):
-        super().extract()
-
         driver = extractors.Selenium.initialize_new_instance()
 
         driver.get("https://search.dreb.es/?q=ping")
