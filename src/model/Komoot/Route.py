4689988cfbb712b0f8362314f7a48c2d853ca37b
Philipp Drebes <philipp.drebes@gmail.com>
2023-05-12 11:05:29 +0200

    add KomootLoader and updated KomootTransformer

--- src/model/Komoot/Route.py
+++ src/model/Komoot/Route.py
@@ -1,7 +1,7 @@
 class KomootRoute:
 
     def __init__(self, link: str, title: str, difficulty: str, distance: str, elevation_up: str, elevation_down: str,
-                 duration: str, speed: str, gpx_file: str = None):
+                 duration: str, speed: str, gpx_file: str | None = None):
         self.link = link
         self.title = title
         self.difficulty = difficulty
